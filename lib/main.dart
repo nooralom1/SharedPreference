68ea163f7ea73da6156dc7be8c87e752b44bd6ce
NS NooR <non377938@gmail.com>
2024-07-30 23:22:31 +0600

    ns4

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 
 import 'package:flutter/material.dart';
-import 'package:sharedpreference/Views/authentication/splash/splash.dart';
+import 'package:sharedpreference/Views/screens/Home.dart';
 
 void main(){
   runApp(const MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Splash(),
+      home: HomePage(),
     );
   }
 }
